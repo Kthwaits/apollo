bc23073ddb9959d456ea773d11f748f2f087a440
Kthwaits <Kthwaits50@gmail.com>
2018-03-04 14:14:59 -0500

    Changed HOST to work dynamically based on if deployed to heroku or not

--- config/app.js
+++ config/app.js
@@ -1,4 +1,8 @@
-module.exports = {
-  // HOST: 'http://localhost:3000'
-  HOST: 'http://whispering-fjord-50685.herokuapp.com'
-};
+if (process.env.NODE_ENV !== 'production') {
+  module.exports = {
+    HOST: 'http://localhost:3000'
+  }
+} else {
+  module.exports = {
+    HOST: 'http://whispering-fjord-50685.herokuapp.com'
+  }}
