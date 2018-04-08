92c0af3d749e29a3eda8eb45e670821a711843d3
Kthwaits <Kthwaits50@gmail.com>
2018-04-07 22:10:45 -0400

    testing socketio on ios

--- actions/profileActions.js
+++ actions/profileActions.js
@@ -13,6 +13,7 @@ const getProfileInfo = (user) => {
         .then((response) => {
           response = response.toJSON();
           user = response.body;
+          user.room = room;
           user.access_token = access_token;
           //user.refresh_token = refresh_token;
           user.socket_id = socket_id;
