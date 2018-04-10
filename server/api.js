a7d8a2eca2c2cadbbf3a193366e44b33a7111979
Kthwaits <Kthwaits50@gmail.com>
2018-04-10 10:57:56 -0400

    testing sync on ios

--- server/api.js
+++ server/api.js
@@ -52,7 +52,7 @@ const exportedApi = (io) => {
                 listeners = Functions.removeFromArray(user, listeners);
               }
               dj = user;
-              io.sockets.to(user.room).emit('updateParty', JSON.stringify(dj), JSON.stringify(listeners));
+              io.sockets.to(user.room).emit('DJInfo', JSON.stringify(dj));
             })
             .catch((err) => {
               console.log(err)
